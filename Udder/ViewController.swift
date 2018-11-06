6e605a46a3ee70a3f71406ea18cfc0fe3b3d34e9
Clay Martin <clay@mobilefit.com>
2018-11-06 07:39:32 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Clay Martin on 11/6/18.
+//  Copyright © 2018 Adaptive Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
